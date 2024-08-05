96a39c6e8485ec91c72d3d2c09b5a88d9c116250
molikk <molikk@github.com>
2024-08-05 17:17:22 +0200

    dev_id 1

--- components/huawei_r4850/huawei_r4850.cpp
+++ components/huawei_r4850/huawei_r4850.cpp
@@ -41,12 +41,13 @@ namespace esphome
       LambdaAction<std::vector<uint8_t>, uint32_t, bool> *lambdaaction;
       canbus::CanbusTrigger *canbus_canbustrigger;
 
-      canbus_canbustrigger = new canbus::CanbusTrigger(this->canbus, device_can_id, 0, true);
+      canbus_canbustrigger = new canbus::CanbusTrigger(this->canbus, 1, 0, true);
       canbus_canbustrigger->set_component_source("canbus");
       App.register_component(canbus_canbustrigger);
       automation = new Automation<std::vector<uint8_t>, uint32_t, bool>(canbus_canbustrigger);
       auto cb = [=](std::vector<uint8_t> x, uint32_t can_id, bool remote_transmission_request) -> void
       {
+        ESP_LOGD(TAG, "Lambda response: %s with data 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X ", can_id, x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]);
         this->on_frame(can_id, remote_transmission_request, x);
       };
       lambdaaction = new LambdaAction<std::vector<uint8_t>, uint32_t, bool>(cb);
